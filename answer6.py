a38d92036e49345eddaf4a69e98a2e651d4125bb
apurva13 <apurva13598@gmail.com>
2018-08-17 13:21:41 +0530

    assignment 2

--- answer6.py
+++ answer6.py
@@ -1,3 +1,10 @@
+#Q6)Find the area of circle pi=3.14
+radius=int(input('Enter radius:'))
+pi=3.14
+area=pi*radius*radius
+print('The area of circle:',area)
+
+
 #Let’s do some interesting exercise:
 name="Tony Stark"
 salary=1000000
